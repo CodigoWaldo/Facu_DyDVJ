c6df4ebbd0b82f534bc6e2c471540b35f084debe
Franco <francoherg@gmail.com>
2024-07-03 00:00:23 -0300

    minijuego 1

--- scenes/Key.gd
+++ scenes/Key.gd
@@ -1,7 +1,10 @@
 extends Area2D
 @onready var player = $"../../CharacterBody2D"
+@onready var audio_stream_player = $AudioStreamPlayer
+@onready var sprite_2d = $Sprite2D
 
-
+var timer = 0
+var start_timer = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,8 +12,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if start_timer:
+		timer+=delta;
+	if timer>0.2:
+		self.queue_free();
 
 func _on_body_entered(body):
 	if body == player:
-		self.queue_free()
+		audio_stream_player.play();
+		sprite_2d.hide();
+		start_timer = true;
+
