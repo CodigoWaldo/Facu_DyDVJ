e4e5b36d191b96e6d4c9a5f89ce6f9a352bd6304
Walter Gabriel Lopez <wglopez100@gmail.com>
2024-07-02 21:58:33 -0300

    assets minigame2 eliminados y lava mas agresiva

--- Scripts/deathzone.gd
+++ Scripts/deathzone.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var initial_position = position.y
-var acceleration = 0.5  # a
+var acceleration = 3  # a
 var vy0=5
 var time_elapsed = 0  # t
 var is_stop=false
@@ -16,14 +16,15 @@ func _physics_process(delta):
 	
 	
 	# Calcular la nueva posición usando la fórmula	
-	if position.y > -1045 and Global.minijuego1Completado == true and not is_stop:
+	if position.y > -1045 and Global.minijuego1Completado and not is_stop:
 		time_elapsed += delta
 		position.y = initial_position -vy0*time_elapsed - 0.5 * acceleration * pow(time_elapsed, 2)
 		
+		if Global.minijuego2Completado:
+			vy0+=0.005
 		
 func _on_body_entered(body):
 	if body.get_name()=="player":
-		print("moriste")
 		die()
 		
 		
